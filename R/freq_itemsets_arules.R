7fb5ede0b4ed71f93f88ac0c00d77243726ad43e
Wander03 <adkerr2001@gmail.com>
2024-11-18 12:57:21 -0800

    progress!

---
+++ R/freq_itemsets_arules.R
@@ -0,0 +1,12 @@
+#' Frequent Itemsets via arules
+#'
+#' [freq_itemsets()] creates K-means model. This engine uses the classical definition
+#' of a K-means model, which only takes numeric predictors.
+#'
+# @includeRmd man/rmd/freq_itemsets_arules.md details
+#'
+#' @name details_freq_itemsets_arules
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
