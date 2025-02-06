ecf7486387c3b6b8a7697dbe01fab7c2316848b6
Wander03 <adkerr2001@gmail.com>
2025-02-06 12:15:25 -0800

    some change

--- R/predict_helpers.R
+++ R/predict_helpers.R
@@ -158,8 +158,8 @@ make_predictions <- function(x, prefix, n_clusters) {
 
   # Extract frequent itemsets and their supports
   items <- attr(object, "item_names")
-  itemsets <- arules::inspect(object)
-  supports <- itemsets$supports
+  frequent_itemsets <- arules::inspect(object)
+  supports <- frequent_itemsets$supports
 
   for (i in seq_len(nrow(new_data))) {
     observed_items <- colnames(new_data)[which(new_data[i, ] == 1)]
@@ -180,6 +180,9 @@ make_predictions <- function(x, prefix, n_clusters) {
         support_full <- relevant_supports[idx]
         support_without <- supports[which(sapply(frequent_itemsets, function(x) identical(x, itemset_without_item)))]
 
+        print(support_full)
+        print(support_without[1])
+
         if (length(support_without) > 0) {
           return(support_full / support_without[1])
         } else {
@@ -196,14 +199,9 @@ make_predictions <- function(x, prefix, n_clusters) {
       print(i)
       print(item)
       print(prob_estimate)
-      print(itemset)
-      print(itemset_without_item)
-      print(support_without)
+      print(frequent_itemsets)
       print(new_data)
       print(probabilities) # WH^Y IS THIS NA?
-      print(support_full)
-      print(support_without[1])
-      print(length(support_without))
       print(missing_items)
       print(new_data[i, item])
     }
