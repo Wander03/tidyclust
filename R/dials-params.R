19c58ae7a447be292bfb9588def8d7c548655414
Wander03 <adkerr2001@gmail.com>
2025-06-19 13:09:28 -0700

    move min_support tuning to dials

--- R/dials-params.R
+++ R/dials-params.R
@@ -43,22 +43,3 @@ values_linkage_method <- c(
   "ward.D", "ward.D2", "single", "complete", "average", "mcquitty", "median",
   "centroid"
 )
-
-#' Min Support
-#'
-#' Used in all `tidyclust::freq_itemsets()` models.
-#'
-#' @inheritParams dials::Laplace
-#' @examples
-#' min_support()
-#' @export
-min_support <- function(range = c(0.1, 0.5), trans = NULL) {
-  dials::new_quant_param(
-    type = "double",
-    range = range,
-    inclusive = c(TRUE, TRUE),
-    trans = trans,
-    label = c(min_support = "Minimum Support Value"),
-    finalize = NULL # Add to look at data and create a CI-like range around some min support value
-  )
-}
