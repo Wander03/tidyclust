1ae32d8e822e87ac256f7828af97e9661db2a718
Wander03 <adkerr2001@gmail.com>
2025-07-03 15:21:34 -0700

    rename `extract_predictions` to `extract_itemset_predictions`
    add example to `extract_itemset_predictions`

--- R/extract_itemset_predictions.R
+++ R/extract_itemset_predictions.R
@@ -7,9 +7,43 @@
 #'
 #' @param pred_output A data frame with one column, where each cell contains a data frame.
 #' @return A data frame with items as columns and non-NA values as rows.
+#'
+#' @examples
+#' toy_df <- data.frame(
+#' "beer"    = c(FALSE, TRUE, TRUE, TRUE, FALSE),
+#' "milk"    = c(TRUE, FALSE, TRUE, TRUE, TRUE),
+#' "bread"   = c(TRUE, TRUE, FALSE, TRUE, TRUE),
+#' "diapers" = c(TRUE, TRUE, TRUE, TRUE, TRUE),
+#' "eggs"    = c(FALSE, TRUE, FALSE, FALSE, FALSE)
+#' )
+#'
+#' new_data <- data.frame(
+#' "beer"    = NA,
+#' "milk"    = TRUE,
+#' "bread"   = TRUE,
+#' "diapers" = TRUE,
+#' "eggs"    = FALSE
+#' )
+#'
+#' fi_spec <- freq_itemsets(
+#'  min_support = 0.05,
+#'  mining_method = "eclat"
+#'  ) |>
+#'  set_engine("arules") |>
+#'  set_mode("partition")
+#'
+#' fi_fit <- fi_spec |>
+#'  fit(~ .,
+#'     data = toy_df
+#'  )
+#'
+#' fi_fit |>
+#'  predict(new_data) |>
+#'  extract_itemset_predictions()
+#'
 #' @export
 
-extract_predictions <- function(pred_output) {
+extract_itemset_predictions <- function(pred_output) {
   # Extract the list of data frames from the single column
   data_frames <- pred_output$.pred_cluster
 
